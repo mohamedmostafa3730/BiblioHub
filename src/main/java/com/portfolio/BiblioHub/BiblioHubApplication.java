84035a87634f0a888b820952092db059d9d40497
MBM3730 <160326122+mohamedmostafa3730@users.noreply.github.com>
2025-10-04 08:13:49 +0300

    Initialize BiblioHub project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/portfolio/BiblioHub/BiblioHubApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.BiblioHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/BiblioHub/BiblioHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.BiblioHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
